9edab5cba0a8b9fdf61d1f6f77f7e7cb3e7fbada
Amir Keshavarz <amirkekh@gmail.com>
2016-08-22 02:38:29 +0430

    init

--- modules/gateways/callback/zarinpalwg.php
+++ modules/gateways/callback/zarinpalwg.php
@@ -18,11 +18,11 @@ $GATEWAY = getGatewayVariables($gatewaymodule);
 if (!$GATEWAY['type']) die('Module Not Activated'); // Checks gateway module is active before accepting callback
 
 /* Get Returned Variables - Adjust for Post Variable Names from your Gateway's Documentation */
-$invoiceid  = $_GET['invoiceid'];
-$Amount 	= $_GET['Amount'];
-$Authority  = $_GET['Authority'];
+$invoiceid = $_GET['invoiceid'];
+$Amount = $_GET['Amount'];
+$Authority = $_GET['Authority'];
 
-$invoiceid  = checkCbInvoiceID($invoiceid, $GATEWAY['name']); // Checks invoice ID is a valid invoice number or ends processing
+$invoiceid = checkCbInvoiceID($invoiceid, $GATEWAY['name']); // Checks invoice ID is a valid invoice number or ends processing
 
 $CaculatedFee = round($Amount * 0.01);
 
@@ -51,11 +51,12 @@ if($_GET['Status'] == 'OK')
 			'Authority' 	 => $Authority,
 			'Amount'	 	 => $Amount + $HiddenFee
 			]);
-			
-		$result  = $resultO->Status; 
+		
+		$result  = $resultO->Status;
 		$transid = $resultO->RefID;
-			
+
 		checkCbTransID($transid); // Checks transaction number isn't already in the database and ends processing if it does
+
 	} catch (Exception $e) {
 		echo '<h2>وقوع وقفه!</h2>';
 		print_r($e);
@@ -65,8 +66,7 @@ if($_GET['Status'] == 'OK')
 	$result = -77;
 }
 
-if($GATEWAY['Currencies'] == 'Rial')
-{
+if($GATEWAY['Currencies'] == 'Rial') {
 	$Amount  *= 10;
 	$PaidFee *= 10;
 }
